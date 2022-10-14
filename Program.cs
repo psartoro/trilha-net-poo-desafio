32d75bb0796ce8c640bf4ac03df7795b7db49d19
Pedro Sartoro <pedrosrt@gmail.com>
2022-10-14 20:57:46 -0300

    smartphone initial

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\nNokia");
+Smartphone nokia = new Nokia("1899999999", "Nokia 5.4", "111111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\nIPhone");
+Smartphone iphone = new Iphone("1899999999", "Iphone 14", "111111111111", 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("GarageBand");
